f1e55c9a22c3e3d626e35a665818c9ee23b49bd9
Jem Davis <jemd321@gmail.com>
2026-03-09 19:36:11 +0000

    problem 2_67

---
+++ chapter2/prob_2_67.c
@@ -0,0 +1,13 @@
+int int_size_is_16() {
+    // Doesn't run properly because << is undefined behaviour for >= w.
+
+    /* Set most significant bit (msb) of 32-bit machine */
+    int set_msb = 1 << 15;
+    set_msb <<= 15;
+    /* Shift past msb of 32-bit word */
+    int beyond_msb = set_msb << 1;
+
+    /* set_msb is nonzero when word size >= 32
+       beyond_msb is zero when word size <= 32 */
+    return set_msb && !beyond_msb;
+}
